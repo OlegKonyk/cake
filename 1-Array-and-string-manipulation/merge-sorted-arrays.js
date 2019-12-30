5bcb6398cf64c9e1ef16bb4433b6ebb4a82d00cd
OlegKonyk <oleg.konyk@aol.com>
2019-12-30 12:04:09 -0800

    updated merge logic and npm dependencies

--- 1-Array-and-string-manipulation/merge-sorted-arrays.js
+++ 1-Array-and-string-manipulation/merge-sorted-arrays.js
@@ -1,21 +1,25 @@
 const expect = require('chai').expect;
 
 function mergeArrays (array_1, array_2) {
-    let array_1_index = 0
-    let array_2_index = 0
     let sorted = [];
-    if(!array_1.length || !array_2.length) {
-        sorted = array_1.length > !array_2.length ? array_1 : array_2;
-        return sorted; 
-    }
-    while( (array_1_index<array_1.length) || (array_2_index<array_2.length) ) {
-        if (array_1[array_1_index] < array_2[array_2_index]) {
+    let totalLength = array_1.length + array_2.length;
+
+    for (let i=0, array_1_index=0, array_2_index=0; i<totalLength; i++) {
+        if (array_1_index in array_1 && array_2_index in array_2) {
+            if(array_1[array_1_index] <= array_2[array_2_index]) {
+                sorted.push(array_1[array_1_index]);
+                array_1_index++;
+            } else if (array_2[array_2_index] < array_1[array_1_index]) {
+                sorted.push(array_2[array_2_index]);
+                array_2_index++;
+            }
+        } else if (array_1_index in array_1) {
             sorted.push(array_1[array_1_index]);
             array_1_index++;
-        } else {
+        } else if (array_2_index in array_2) {
             sorted.push(array_2[array_2_index]);
             array_2_index++;
-        }        
+        }
     }
 
     return sorted;
@@ -30,9 +34,17 @@ describe('mergeArrays', function() {
         expect(expected).to.deep.equal(actual);
     })
 
-    it('Different length arrays', function() {
+    it('Different length arrays [array2 longer]', function() {
         let array1 = [3, 4, 6, 10];
         let array2 = [1, 5, 8, 12, 14, 19];
+        let actual = mergeArrays(array1, array2);
+        let expected = [1, 3, 4, 5, 6, 8, 10, 12, 14, 19];
+        expect(expected).to.deep.equal(actual);
+    })
+
+    it('Different length arrays [array1 longer]', function() {
+        let array1 = [1, 5, 8, 12, 14, 19];
+        let array2 = [3, 4, 6, 10];
         let actual = mergeArrays(array1, array2);
         let expected = [1, 3, 4, 5, 6, 8, 10, 12, 14, 19];
         expect(expected).to.deep.equal(actual);
